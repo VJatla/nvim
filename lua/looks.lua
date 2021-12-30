81691efdc9a7c55a63a41c476616acfcfda44f83
vj@aurora <venkatesh.jatla@gmail.com>
2021-12-30 14:28:21 -0700

    Add color scheme in lua/this_sys_settings.lua

--- lua/looks.lua
+++ lua/looks.lua
@@ -2,9 +2,6 @@
 vim.opt.number = true
 vim.opt.colorcolumn = "80"
 
--- Setting theme to gruvbox
--- vim.o.background = "dark"
-vim.cmd([[colorscheme nord]])
 
 -- GUI font
 vim.cmd 'set guifont=FiraCode-Retina:h15'
